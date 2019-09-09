84f78e9522ac42f6fa086c55ff6bcb19b3c05890
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-09 18:54:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 function deleteFromObjectByKey(object, key){
 let newObject =  Object.assign({}, object);
 
-newObject.key = undefined;
+newObject.key = "undefined";
 return newObject;
 }
 
