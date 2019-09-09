4bade64225cb5ecdc722c70fd5c0d3da56c55a6f
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-09 18:05:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var clone = Object.assing({}, object)
+  var clone = Object.assign({}, object)
   delete clone.key;
   return clone;
 
