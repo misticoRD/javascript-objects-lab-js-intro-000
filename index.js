f1b800dca095ee7f3c03e14d02061bc7a4c10b92
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-09 17:41:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object;
 }
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, { [key]: value })
+}
