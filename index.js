7806f7ecb2063352f8b2175ac1eb26b55f79fed4
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-09 17:37:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ function updateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object;
 }
+
+function  updateObjectWithKeyAndValue(object, key, value){
+  let newObject = Object.assing({},object, key, value)
+  return newObject;
+
+}
